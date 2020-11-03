d9c49db029f8d1e9abade233c49a1bd77db0022a
20centcroak <vpaveau@outook.com>
2020-11-03 11:26:06 +0100

    manage dependencies in setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyDocxReport", 
-    version="0.0.2",
+    version="0.0.3",
     author="20centCroak",
     author_email="",
     description="module to ease the docx report generation using templates and importing features",
@@ -19,4 +19,8 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+          'pandas',
+          'python-docx'
+      ],
 )
